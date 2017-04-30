cc8fdfa054437db4d514ef43b34843310d16b0c2
dominika skoczen <skoczendo@poczta.onet.pl>
2017-04-30 15:10:53 +0200

    category crud

---
+++ my_app/src/AppBundle/Form/CategoryType.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Category type.
+ */
+namespace AppBundle\Form;
+
+use AppBundle\Entity\Category;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Class CategoryType.
+ *
+ * @package AppBundle\Form
+ */
+class CategoryType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'name',
+            TextType::class,
+            [
+                'label' => 'label.name',
+                'required' => true,
+                'attr' => [
+                    'max_length' => 128,
+                ],
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => Category::class,
+                'validation_groups' => 'category-default',
+            ]
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'category_type';
+    }
+}
